9fccf4b897878cf02f636162f1ca340ef0646449
Krishna-J005 <krishnajaiswal17232@gmail.com>
2024-01-07 17:07:37 +0530

    another commit

--- src/components/CryptoDataTable.js
+++ src/components/CryptoDataTable.js
@@ -1,6 +1,53 @@
 // CryptoDataTable.js
 import React, { useState, useEffect } from 'react';
 import './CryptoDataTable.css';
+let arr = [
+    {
+        cryptoName: "Bitcoin",
+        cryptoId: "BTC",
+        price: 44024,
+        logo: "https://assets.coingecko.com/coins/images/1/standard/bitcoin.png?1696501400",
+        netChange24h: 120,
+        netChange7d: -500,
+
+    },
+    {
+        cryptoName: "Ethereum",
+        cryptoId: "ETH",
+        price: 4402,
+        logo: "https://assets.coingecko.com/coins/images/279/standard/ethereum.png?1696501628",
+        netChange24h: 120,
+        netChange7d: 50,
+
+    },
+    {
+        cryptoName: "DogeCoin",
+        cryptoId: "DOGE",
+        price: 440,
+        logo: "https://assets.coingecko.com/coins/images/5/standard/dogecoin.png?1696501409",
+        netChange24h: 12,
+        netChange7d: 100,
+
+    },
+    {
+        cryptoName: "Shiba Inu",
+        cryptoId: "SHIB",
+        price: 0.0054,
+        logo: "https://assets.coingecko.com/coins/images/11939/standard/shiba.png?1696511800",
+        netChange24h: 0.0,
+        netChange7d: -0.00500,
+
+    },
+    {
+        cryptoName: "BNB",
+        cryptoId: "BNB",
+        price: 308.28,
+        logo: "https://assets.coingecko.com/coins/images/825/standard/bnb-icon2_2x.png?1696501970",
+        netChange24h: -12,
+        netChange7d: -20,
+
+    },
+]
 
 const CryptoDataTable = () => {
     const [cryptoData, setCryptoData] = useState([]);
@@ -11,56 +58,10 @@ const CryptoDataTable = () => {
 
     useEffect(() => {
         // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint
-        setTimeout(() => {
-            let arr = [
-                {
-                    cryptoName: "Bitcoin",
-                    cryptoId: "BTC",
-                    price: 44024,
-                    logo: "https://assets.coingecko.com/coins/images/1/standard/bitcoin.png?1696501400",
-                    netChange24h: 120,
-                    netChange7d: -500,
-
-                },
-                {
-                    cryptoName: "Ethereum",
-                    cryptoId: "ETH",
-                    price: 4402,
-                    logo: "https://assets.coingecko.com/coins/images/279/standard/ethereum.png?1696501628",
-                    netChange24h: 120,
-                    netChange7d: 50,
-
-                },
-                {
-                    cryptoName: "DogeCoin",
-                    cryptoId: "DOGE",
-                    price: 440,
-                    logo: "https://assets.coingecko.com/coins/images/5/standard/dogecoin.png?1696501409",
-                    netChange24h: 12,
-                    netChange7d: 100,
-
-                },
-                {
-                    cryptoName: "Shiba Inu",
-                    cryptoId: "SHIB",
-                    price: 0.0054,
-                    logo: "https://assets.coingecko.com/coins/images/11939/standard/shiba.png?1696511800",
-                    netChange24h: 0.0,
-                    netChange7d: -0.00500,
-
-                },
-                {
-                    cryptoName: "BNB",
-                    cryptoId: "BNB",
-                    price: 308.28,
-                    logo: "https://assets.coingecko.com/coins/images/825/standard/bnb-icon2_2x.png?1696501970",
-                    netChange24h: -12,
-                    netChange7d: -20,
-
-                },
-            ]
+        const id = setTimeout(() => {
             setCryptoData(arr);
-        }, 100)
+        }, 100);
+        return (() => clearTimeout(id));
     }, []);
 
     const handleSort = (column) => {
@@ -84,11 +85,11 @@ const CryptoDataTable = () => {
                     <tr>
                         <th>#</th>
                         <th class="sortable" onClick={() => handleSort('cryptoName')}>Coin </th>
-                        <th class="sortable"  onClick={() => handleSort('price')}>Price</th>
-                        <th class="sortable"  onClick={() => handleSort('netChange24h')}>
+                        <th class="sortable" onClick={() => handleSort('price')}>Price</th>
+                        <th class="sortable" onClick={() => handleSort('netChange24h')}>
                             Net Change (24h)
                         </th>
-                        <th class="sortable"  onClick={() => handleSort('netChange7d')}>
+                        <th class="sortable" onClick={() => handleSort('netChange7d')}>
                             Net Change (7d)
                         </th>
                     </tr>
@@ -96,11 +97,11 @@ const CryptoDataTable = () => {
                 <tbody>
                     {sortedCryptoData.map((crypto, index) => (
                         <tr key={crypto.cryptoId}>
-                            <td>{index+1}</td>
+                            <td>{index + 1}</td>
                             <td>
                                 <div className='middle'>
-                                    {crypto.cryptoName}
-                                    <img loading="lazy" style={{marginLeft: '8px'}} src={crypto.logo} alt={`${crypto.cryptoName} Logo`} width="24px"></img>
+                                    <img loading="lazy" style={{ marginRight: '8px' }} src={crypto.logo} alt={`${crypto.cryptoName} Logo`} width="24px"></img>
+                                    <span style={{ marginRight: '8px' }}>{crypto.cryptoName} </span>  ({crypto.cryptoId})
                                 </div>
                             </td>
                             <td className='numberAlignment'>
